4d8a3d0d800996d567ab8b66fca36957f1484386
Jyoti Chaudhary <jyotikrjc@gmail.com>
2020-10-29 16:18:05 +0530

    Sorting and Searching

---
+++ CSES2/ConcertTickets.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define lli long long int
+#define l long int
+#define ld long double
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    lli n, m, curr, ticket;
+    cin >> n >> m;
+    multiset<lli, greater<int>> tickets;
+ 
+    while (n--)
+    {
+        cin >> ticket;
+        tickets.insert(ticket);
+    }
+ 
+    while (m--)
+    {
+        cin >> curr;
+        auto it = tickets.lower_bound(curr);
+        if (it == tickets.end())
+        {
+            cout << -1 << endl;
+        }
+        else
+        {
+            cout << *it << endl;
+            tickets.erase(it);
+        }
+    }
+    return 0;
+}
