bb404d0228c55b00eed249cb5d1a09dbfa6991e2
Jyoti Chaudhary <jyotikrjc@gmail.com>
2021-01-09 15:50:23 +0530

    Combinatorics

---
+++ ncr.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int mod =  1000000007;
+typedef long long ll ;
+int fac[100005];
+ll power(ll x, ll n, ll m=mod){
+    ll res = 1;
+    while(n){
+        if(n&1){
+            res = res * x % m;
+        }
+        x = x * x % m;
+        n>>=1;
+    }
+    return (res%m);
+}
+
+int ncr(int n,int r)
+{
+    return fac[n]*((power(fac[n-r], mod-2)*power(fac[r], mod-2))%mod)%mod;
+}
